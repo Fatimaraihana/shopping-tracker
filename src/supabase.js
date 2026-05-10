7863e5e12775823778d07ced08b8c5b9ee5eb663
Fathima Raihana <ryefatima@gmail.com>
2026-05-10 12:42:16 +0530

    migrated from firebase to supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "qdejgvtnsouaptxmhrfg";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZWpndnRuc291YXB0eG1ocmZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTM3MzQsImV4cCI6MjA5Mzk2OTczNH0.AsvpBeMLbPSd8ZVtLbKKAK2i5gdEzLlo1D-eZ_KW2rA";
+const supabaseUrl = "https://qdejgvtnsouaptxmhrfg.supabase.co";
+const supabaseKey = "sb_publishable_7_yJR4XUNkKqOCrqji7UDQ_HL247bbx";
 
 export const supabase = createClient(
   supabaseUrl,
